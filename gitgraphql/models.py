db1471ad58b7673ab08710e13f2eb05f6f716d87
Oleh Hryshchuk <olehhryshchuk@gmail.com>
2021-09-24 23:47:18 +0300

    "submit" button

--- gitgraphql/models.py
+++ gitgraphql/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    github_login = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.github_login + "ABLALALABLA"
